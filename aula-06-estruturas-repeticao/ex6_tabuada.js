eff0c0b5f5baa84ec3e3d7f7b9fe302fb7e0c374
nandafolgoni <folgonif@gmail.com>
2026-05-25 11:30:01 -0300

    aleatorio

---
+++ aula-06-estruturas-repeticao/ex6_tabuada.js
@@ -0,0 +1,16 @@
+// rodas rpm install prompt-sync
+
+// Importar o módulo (Para rodar algoritmos no terminal)
+const prompt = require ('prompt-sync')()
+
+// Título
+console.log('--- Tabuada ---')
+console.log("")
+
+// Perguntar (qual tabuada gerar)
+let numero = Number(prompt('Digite um número para gerar a tabuada: '))
+
+// Laço para gerar a tabuada
+for (let i = 1; i <= 10; i++){
+    console.log(numero + "x" + i + "=" + (numero * i))
+}
