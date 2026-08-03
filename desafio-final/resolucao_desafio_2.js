aa6403e2ae4873e3b1bdc4b2c5fbdcae42de478b
nandafolgoni <folgonif@gmail.com>
2026-08-03 11:09:57 -0300

    finalizado
    arquivos finais

--- desafio-final/resolucao_desafio_2.js
+++
@@ -1,6 +0,0 @@
-// Não esquecer de instalar (Para poder simular):
-// npm install prompt-sync
-
-const prompt = require('prompt-sync')();
-
-// Desenvolver a codificação do 2º desafio à partir deste ponto
